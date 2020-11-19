fe1e1f3d877337ece850ba75ab131f13fba713ea
Esin Tekin <esnntekin@gmail.com>
2020-11-19 21:37:42 +0300

    Initial commit

---
+++ Project1/Controllers/Default.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project1.Controllers
+{
+    public class Default : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project1/Controllers/Parents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project1.Controllers
+{
+    public class Parents : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NewUsersPage()
+        {
+
+            return View();
+        }
+
+        public IActionResult NewUsersPage(FormCollection form)
+        {
+            return View();
+        }
+    }
+}
